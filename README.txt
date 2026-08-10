e3cca4b2e4574220db04c7805ed3dd5161d2ace3
hdeveney <h.deveney@outlook.fr>
2026-08-10 02:05:17 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,1 +1,15 @@
-TAEKWONDO FACILE V4 — illustrations de positions retravaillées avec personnage plein en dobok, moins schématique.
+TAEKWONDO FACILE V5 — POSITIONS HUMAINES
+
+- anciennes silhouettes SVG supprimées
+- 6 illustrations humaines réalistes intégrées
+- Naranhi-seogi
+- Ap-seogi
+- Apkubi
+- Dwitkubi
+- Juchum-seogi
+- Kkoa-seogi
+- quiz visuel mis à jour
+- cache GitHub Pages versionné pour forcer la mise à jour
+
+Pour le dépôt GitHub Samuel :
+téléverser tous les fichiers de cette archive à la racine et remplacer les fichiers existants.
--- sw.js
+++ sw.js
@@ -1,2 +1,4 @@
-const C='taekwondo-facile-v4-fullbody',A=["./", "./index.html", "./manifest.webmanifest", "./tkd_garde.gif", "./tkd_salut.gif", "./naranhi_seogi.svg", "./ap_seogi.svg", "./ap_kubi.svg", "./dwit_kubi.svg", "./juchum_seogi.svg", "./kkoa_seogi.svg"];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='taekwondo-facile-v5-humain',A=["./", "./index.html", "./manifest.webmanifest", "./tkd_garde.gif", "./tkd_salut.gif", "./naranhi_seogi.png", "./ap_seogi.png", "./ap_kubi.png", "./dwit_kubi.png", "./juchum_seogi.png", "./kkoa_seogi.png"];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
